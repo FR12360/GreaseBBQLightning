224c1fd5c1bb880684e969412f8bd473c3a4273f
murphyj169 <murphyj169@gmail.com>
2020-01-03 14:13:31 -0600

    Fix RandP Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpMecanumOneDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpMecanumOneDriver.java
@@ -8,7 +8,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 import edu.spa.ftclib.internal.drivetrain.MecanumDrivetrain;
-
+import edu.spa.ftclib.internal.state.ToggleBoolean;
+import edu.spa.ftclib.internal.state.Button;
 
 //@Disabled
 @TeleOp(name="One Driver Mecanum", group="Mecanum Drivetrain")
@@ -17,10 +18,13 @@ public class teleOpMecanumOneDriver extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
     private greaseBBQLightning robot = new greaseBBQLightning();
+    private Button RNPFixStart;
     public MecanumDrivetrain drivetrain;
 
     @Override
     public void runOpMode() {
+        RNPFixStart = new Button();
+
         robot.init(hardwareMap);
         drivetrain = new MecanumDrivetrain(new DcMotor[]{robot.myBigMotorFrontLeft, robot.myBigMotorFrontRight, robot.myBigMotorBackLeft, robot.myBigMotorBackRight});
 
@@ -50,37 +54,43 @@ public class teleOpMecanumOneDriver extends LinearOpMode {
             final double rr = gamepad1.left_stick_y - gamepad1.left_stick_x + (gamepad1.right_stick_x * .7f);
 
             robot.myBigMotorFrontLeft.setPower(Range.clip(lf, -1, 1) * .5);
-            robot.myBigMotorFrontRight.setPower(Range.clip(rf, -1, 1) * 5);
-            robot.myBigMotorBackLeft.setPower(Range.clip(lr, -1, 1) * 5);
-            robot.myBigMotorBackRight.setPower(Range.clip(rr, -1, 1) * 5);
+            robot.myBigMotorFrontRight.setPower(Range.clip(rf, -1, 1) * .5);
+            robot.myBigMotorBackLeft.setPower(Range.clip(lr, -1, 1) * .5);
+            robot.myBigMotorBackRight.setPower(Range.clip(rr, -1, 1) * .5);
 
+            ////////////////////////////////////////
             //Rack and Pinion Motor Control
+            ///////////////////////////////////////
 
-            //Leave the code below commented until fixed. It will be used to reset the RandP motor
-            //if it ever gets stuck in the wrong position.
-            /**Fix R and P when stuck in up position
-             if(this.gamepad1.left_bumper){
-             reset = true;
-             }
+            ////////////////////////////////////////
+            //Fix R and P when stuck in up position
+            ///////////////////////////////////////
 
-             while(reset){
-             robot.myBigMotorRandP.setPower(-this.gamepad1.left_trigger);
-             }
-             */
+            RNPFixStart.input(this.gamepad1.dpad_left);
 
-            while (this.gamepad1.right_trigger > 0 && robot.myBigMotorRandP.getCurrentPosition() <= 3500 && opModeIsActive()) {
+            if(RNPFixStart.isPressed()){
+                robot.myBigMotorRandP.setPower(-.3);
+            }
+
+            if(RNPFixStart.onRelease()) {
+               robot.myBigMotorRandP.setPower(0);
+               robot.myBigMotorRandP.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+               robot.myBigMotorRandP.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }
+
+            ///////////////////////////////////
+            //End Fix R and P when Stuck
+            ///////////////////////////////////
+           while (this.gamepad1.right_trigger > 0 && robot.myBigMotorRandP.getCurrentPosition() <= 3500 && opModeIsActive()) {
                 if (robot.myBigMotorRandP.getCurrentPosition() >= 3500) {
                     robot.myBigMotorRandP.setPower(0);
                     break;
                 }
 
-                robot.myBigMotorRandP.setPower(this.gamepad1.right_trigger / 2);
+                robot.myBigMotorRandP.setPower(this.gamepad1.right_trigger / 2.5);
 
             }
 
-
-            //Set RandP motor power to zero just as a safe guard when nothing is being pressed
-            //robot.myBigMotorRandP.setPower(0);
 
             while (this.gamepad1.left_trigger > 0 && robot.myBigMotorRandP.getCurrentPosition() >= 0 && opModeIsActive()) {
                 if (robot.myBigMotorRandP.getCurrentPosition() <= 0) {
@@ -88,35 +98,22 @@ public class teleOpMecanumOneDriver extends LinearOpMode {
                     break;
                 }
 
-                robot.myBigMotorRandP.setPower(-this.gamepad1.left_trigger / 2);
+                robot.myBigMotorRandP.setPower(-this.gamepad1.left_trigger / 2.5);
             }
 
             //Set RandP motor power to zero just as a safe guard when nothing is being pressed
-            robot.myBigMotorRandP.setPower(0);
+            //if(!RNPFixStart.Press())
+                robot.myBigMotorRandP.setPower(0);
 
-            /**
-             //Turn on rear stone lifters
-             if(this.gamepad1.a){
-             double power = robot.myBigMotorLeftLifter.getPower();
 
-             if(power < 1) {
-             power = power + .1;
-             robot.myBigMotorLeftLifter.setPower(power);
-             robot.myBigMotorRightLifter.setPower(power);
-             }
-             }
 
-             //Turn off rear lifters
-             if(this.gamepad1.b){
-             double power = robot.myBigMotorLeftLifter.getPower();
+            ////////////////////////////////////
+            //End Rack and Pinion Control
+            ///////////////////////////////////
 
-             if(power > 0) {
-             power = power - .1;
-             robot.myBigMotorLeftLifter.setPower(power);
-             robot.myBigMotorRightLifter.setPower(power);
-             }
-             }
-             */
+            ///////////////////////////////
+            //Claw Control
+            ///////////////////////////////
             if (this.gamepad1.right_bumper) {
                 robot.myBigServoClaw.setPower(-1);
             }
@@ -125,6 +122,13 @@ public class teleOpMecanumOneDriver extends LinearOpMode {
                 robot.myBigServoClaw.setPower(.3);
             }
 
+            ///////////////////////////////
+            //End Claw Control
+            //////////////////////////////
+
+            /////////////////////////////
+            //Foundation Control
+            ////////////////////////////
             if (this.gamepad1.y) {
                 robot.myBigServoFoundation.setPower(-1);
             }
@@ -132,10 +136,13 @@ public class teleOpMecanumOneDriver extends LinearOpMode {
             if (this.gamepad1.x) {
                 robot.myBigServoFoundation.setPower(0);
             }
+            ///////////////////////////
+            //End Foundation Control
+            //////////////////////////
 
-            //telemetry.addData("Stone Intake Power", robot.myBigMotorLeftLifter.getPower());
-            telemetry.addData("Rack and Pinion Motor Position", "%7d",
-                    robot.myBigMotorRandP.getCurrentPosition());
+            telemetry.addData("RNP Motor Position", "%7d", robot.myBigMotorRandP.getCurrentPosition());
+            telemetry.addData("RNP Motor Speed Up", this.gamepad1.right_trigger);
+            telemetry.addData("RNP Motor Speed Down", this.gamepad1.left_trigger);
             telemetry.update();
         }
     }
